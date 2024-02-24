363d973a856bef2310e4f9e86f10a443c3a31a86
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2024-02-24 23:58:05 +0200

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Task-Manager-Redux/",
   build: {
     outDir: "dist",
   },
